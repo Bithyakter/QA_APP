c2bba3ef7ecd7116a632c4023feddf8372c614fc
Mst.Bithy Akter <mstbithy@outlook.com>
2024-06-07 21:55:34 +0600

    update

---
+++ QA_Web/Controllers/AnswersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QA_Web.Controllers
+{
+    public class AnswersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateAnswer()
+        {
+            return View();
+        }
+    }
+}
---
+++ QA_Web/Controllers/QuestionsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QA_Web.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateQuestion()
+        {
+            return View();
+        }
+    }
+}
